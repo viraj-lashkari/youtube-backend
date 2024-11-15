4a05123049329cfdff21700b891128c2f2836612
viraj-lashkari <work.virajdas@gmail.com>
2024-11-15 18:46:51 +0530

    comment the console.log in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // return res
 
   const { fullName, email, username, password } = req.body;
-  console.log("email: ", email);
+  // console.log("email: ", email);
 
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
